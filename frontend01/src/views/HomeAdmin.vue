11c398ca635106cd7bab1acced245da18dfd11bf
RENZ-MAVERICK-FAMA <102272777+RENZ-MAVERICK-FAMA@users.noreply.github.com>
2024-09-22 18:27:11 +0800

    Update HomeAdmin.vue

--- frontend01/src/views/HomeAdmin.vue
+++ frontend01/src/views/HomeAdmin.vue
@@ -82,7 +82,7 @@
 <script>
 import axios from 'axios';
 import logos from './base64-logos.json';
-import pdfMake from 'pdfmake/build/pdfmake';
+import 'pdfmake/build/pdfmake';
 import 'pdfmake/build/vfs_fonts'
 // pdfMake.vfs = pdfMake.vfs;
 console.log('pdfMake:', pdfMake);
