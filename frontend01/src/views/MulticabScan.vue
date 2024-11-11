b28ed76fa65d4be7ae9a001f8987082ca04c7bda
RENZ-MAVERICK-FAMA <102272777+RENZ-MAVERICK-FAMA@users.noreply.github.com>
2024-11-11 21:37:15 +0800

    labels

--- frontend01/src/views/MulticabScan.vue
+++ frontend01/src/views/MulticabScan.vue
@@ -496,7 +496,7 @@ this.sortedUnits.forEach((unit, index) => {
     doc.setFontSize(12);
  // Set initial coordinates for positioning
  const labelX = 100; 
-const labelxy = 150;      // X position for labels
+const labelxy = 130;      // X position for labels
 const countX = 160;       // X position for count values
 const amountX = 180;      // X position for amount values
 const headerY = startY + 30;  // Y position for the header row
