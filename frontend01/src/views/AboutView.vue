be5139fd85ab60472a7d1069e2149a346682a50b
rajarh-cas <161417601+rajarh-cas@users.noreply.github.com>
2024-11-20 19:03:02 +0800

    Add files via upload

--- frontend01/src/views/AboutView.vue
+++ frontend01/src/views/AboutView.vue
@@ -1,16 +1,55 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <main class="flex justify-center items-center bg-gray-100">
+    <div class="bg-white mt-10 w-[1200px] p-4 shadow-lg px-10">
+      <div class="flex justify-center mt-10">
+        <h2 class="text-center text-[40px] font-bold mb-4 items-center text-green-600">QRMCPASS</h2>
+      <img src="../assets/ceedmo-logo.png" alt="" class="w-[70px] h-[50px] ml-5 ">
+      </div>
+      <p class="text-center mb-4">In a world that seems to move faster every day, the small moments of peace and quiet often go unnoticed. Whether it's the brief silence between raindrops on a window or the gentle rustling of leaves in the early morning, these tiny instances offer a calming pause amidst the chaos. They serve as reminders of simplicity, grounding us in the present moment and helping us recharge. Taking a step back to observe these details can bring unexpected clarity, showing us that beauty exists even in the most ordinary scenes of our everyday lives.</p>
+      
+      <!-- eldwyn -->
+      <div class=" justify-around mt-10 grid grid-rows-4 grid-cols-1 md:grid md:grid-rows-1 md:grid-cols-4 gap-2">
+        <div class="flex justify-center items-center w-[260px] bg-green-500 p-4 rounded-lg">
+          <div class="flex flex-col justify-center items-center w-full">
+            <img src="..\assets\city-logo.png" alt="eldywn" class="w-16 h-16 rounded-full mb-4">
+          <h3 class="mb-4">Eldwyd James B. Dano</h3>
+          <p class="text-justify mb-4">I focus on blending creativity with technology to deliver visually appealing and functional designs. Whether it’s a small website or a large application, my goal is to build seamless experiences that meet user needs and business goals.</p>
+          </div>
+        </div>
+
+        <!-- teofilo -->
+        <div class=" flex justify-center items-center w-[260px] bg-green-500 p-4 rounded-lg">
+          <div class="flex flex-col justify-center items-center w-full">
+            <img src="..\assets\city-logo.png" alt="teofilo" class="w-16 h-16 rounded-full mb-4">
+          <h3 class="mb-4">Teofilo P. Gonzaga III</h3>
+          <p class="text-justify mb-4">I focus on blending creativity with technology to deliver visually appealing and functional designs. Whether it’s a small website or a large application, my goal is to build seamless experiences that meet user needs and business goals.</p>
+          </div>
+        
+        </div>
+
+        <!-- maverick -->
+        <div class=" flex justify-center items-center w-[260px] bg-green-500 p-4 rounded-lg">
+          <div class="flex flex-col justify-center items-center w-full">
+            <img src="..\assets\city-logo.png" alt="maverick" class="w-16 h-16 rounded-full mb-4">
+          <h3 class="mb-4">Renz Maverick Fama</h3>
+          <p class="text-justify mb-4">
+            I focus on blending creativity with technology to deliver visually appealing and functional designs. Whether it’s a small website or a large application, my goal is to build seamless experiences that meet user needs and business goals.
+          </p>
+          </div>
+         
+        </div>
+        
+        <!-- jaharrah -->
+        <div class="flex justify-center items-center w-[260px] bg-green-500 p-4 rounded-lg">
+        <div class="flex flex-col justify-center items-center w-full">
+          <img src="../assets/jaharrah.png" alt="jaharrah" class="w-16 h-16 rounded-full mb-4" />
+          <h3 class="mb-4">Jaharrah L. Caseres</h3>
+          <p class="text-justify mb-4">
+            I focus on blending creativity with technology to deliver visually appealing and functional designs. Whether it’s a small website or a large application, my goal is to build seamless experiences that meet user needs and business goals.
+          </p>
+        </div>
   </div>
+      </div>
+    </div>
+  </main>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
-
